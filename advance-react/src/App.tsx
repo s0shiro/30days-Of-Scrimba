ce19b3f324d0af2983998df9da487b3b643908fc
Nielven Mascariñas <mascarinas022@gmail.com>
2024-10-25 11:43:15 +0800

    added a another example of using the headless Toggle components

--- advance-react/src/App.tsx
+++ advance-react/src/App.tsx
@@ -1,46 +1,27 @@
 import "./App.css";
-// import Avatar from "./components/Avatar";
-// import Button from "./components/Button";
-// import { CiShoppingCart } from "react-icons/ci";
 import Menu from "./components/Menu/index";
+import Star from "./components/Star";
 import Toggle from "./components/Toggle/index";
 
 function App() {
-  //   const handleClick = () => console.log("Buying...");
-
   return (
     <>
-      {/* <Button
-        size="lg"
-        variant="success"
-        className="green"
-        onClick={handleClick}
-      >
-        <CiShoppingCart /> Buy now!
-      </Button>
-
-      <Avatar
-        src="https://scontent.fmnl4-6.fna.fbcdn.net/v/t39.30808-6/451418027_2164615280578350_86385005731237855_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG-CtnuZXTvRgJPaXROMny9R7XuKmkIKixHte4qaQgqLLMYMoa7pEnEBF61JXu3vHm9CmogSQjiSI8GLGRDkwEY&_nc_ohc=gnWFk5l16KIQ7kNvgFp8KBd&_nc_zt=23&_nc_ht=scontent.fmnl4-6.fna&_nc_gid=AaOxdb-6vtBRF_cC7pVNGIk&oh=00_AYBVmPGPc3hES66tRHrbYAUW3iWafhSgCcjKhu-YiGW1lA&oe=671F8224"
-        alt="Bob Ziroll"
-      />
-      <br />
-      <Avatar>NM</Avatar>
-      <br />
-      <Avatar /> */}
-
+      <Star />
       {/*  */}
       <Toggle>
-        <Toggle.Button>
-          <Menu.Button>Menu</Menu.Button>
-        </Toggle.Button>
-        <Toggle.On>
-          <Menu.Dropdown>
-            <Menu.Item>Home</Menu.Item>
-            <Menu.Item>About</Menu.Item>
-            <Menu.Item>Contact</Menu.Item>
-            <Menu.Item>Services</Menu.Item>
-          </Menu.Dropdown>
-        </Toggle.On>
+        <Menu>
+          <Toggle.Button>
+            <Menu.Button>Menu</Menu.Button>
+          </Toggle.Button>
+          <Toggle.On>
+            <Menu.Dropdown>
+              <Menu.Item>Home</Menu.Item>
+              <Menu.Item>About</Menu.Item>
+              <Menu.Item>Contact</Menu.Item>
+              <Menu.Item>Services</Menu.Item>
+            </Menu.Dropdown>
+          </Toggle.On>
+        </Menu>
       </Toggle>
     </>
   );
---
+++ advance-react/src/components/Star.tsx
@@ -0,0 +1,17 @@
+import Toggle from "./Toggle/index";
+import { BsStar, BsStarFill } from "react-icons/bs";
+
+export default function Star() {
+  return (
+    <Toggle>
+      <Toggle.Button>
+        <Toggle.On>
+          <BsStarFill className="star filled" />
+        </Toggle.On>
+        <Toggle.Off>
+          <BsStar className="star" />
+        </Toggle.Off>
+      </Toggle.Button>
+    </Toggle>
+  );
+}
